aca2f50cb5989521e7d41241093f74f618f743a4
Silicium <103604296+siliciium@users.noreply.github.com>
2025-08-27 16:00:17 +0200

    Create AssemblyInfo.cpp

---
+++ freetrack20enhanced_wrapper/AssemblyInfo.cpp
@@ -0,0 +1,24 @@
+#include "pch.h"
+
+using namespace System;
+using namespace System::Reflection;
+using namespace System::Runtime::CompilerServices;
+using namespace System::Runtime::InteropServices;
+using namespace System::Security::Permissions;
+
+[assembly:AssemblyTitleAttribute(L"Freetrack 2.0 Enhanced")];
+[assembly:AssemblyDescriptionAttribute(L"Freetrack 2.0 Enhanced Protocol")];
+[assembly:AssemblyConfigurationAttribute(L"")];
+[assembly:AssemblyCompanyAttribute(L"Silicium.")];
+[assembly:AssemblyProductAttribute(L"Freetrack 2.0 Enhanced")];
+[assembly:AssemblyCopyrightAttribute(L"Copyright (c)  2025")];
+[assembly:AssemblyTrademarkAttribute(L"")];
+[assembly:AssemblyCultureAttribute(L"")];
+
+
+[assembly:AssemblyVersionAttribute(L"1.0.0")];
+
+[assembly:ComVisible(false)];
+
+// addded by me
+[assembly:CLSCompliantAttribute(true)] ;
