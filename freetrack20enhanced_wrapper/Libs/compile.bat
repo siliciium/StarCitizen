66ddd378e7ff9901585ee46aa9d84483142d42eb
Silicium <103604296+siliciium@users.noreply.github.com>
2025-08-28 14:25:35 +0200

    Update compile.bat

--- freetrack20enhanced_wrapper/Libs/compile.bat
+++ freetrack20enhanced_wrapper/Libs/compile.bat
@@ -13,14 +13,12 @@ cd ..
 cl /MD /EHsc /c freetrack20enhanced.cpp
 lib /OUT:freetrack20enhanced.lib freetrack20enhanced.obj
 
-cd ..
-cl /EHsc test_freetrack.cpp compat\shm.lib
-
 REM require (not loaded by star citizen)
 REM reg add "HKCU\Software\Freetrack\FreetrackClient" /v Path /t REG_SZ /d "D:/Users/[--USERNAME--]/test_joystick/bin/" /f
 
 REM require (loaded by star citizen : NPClient64.dll)
 REM reg add "HKEY_CURRENT_USER\Software\NaturalPoint\NATURALPOINT\NPClient Location" /v Path /t REG_SZ /d "D:/Users/[--USERNAME--]/test_joystick/bin/" /f
 
-
-cl /std:c++latest /EHsc test_joystick4.cpp freetrack20enhanced.lib compat\shm.lib
+REM cd ..
+REM cl /EHsc test_freetrack.cpp compat\shm.lib
+REM cl /std:c++latest /EHsc test_joystick4.cpp freetrack20enhanced.lib compat\shm.lib
