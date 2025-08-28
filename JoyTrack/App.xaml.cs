792f6afb07868a60923413715da868af721bcc05
Silicium <103604296+siliciium@users.noreply.github.com>
2025-08-28 14:47:08 +0200

    Create App.xaml.cs

---
+++ JoyTrack/App.xaml.cs
@@ -0,0 +1,11 @@
+using System.Windows;
+
+namespace JoyTrack
+{
+    /// <summary>
+    /// Logique d'interaction pour App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
